3616e8c4415dc3199734fe670c4091be2b981e18
yaayyuhalaziz <yaayyuhalaziz@gmail.com>
2019-12-02 20:56:22 +0700

    add kasus 1

--- kemampuan-dasar/kemampuan-dasar-1/kasus/cari-java.sh
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/cari-java.sh
@@ -8,9 +8,11 @@ echo -e "\nHasil Pencarian file .java pada direktori: $dir\n"
 if [ ! -d "$dir" ]
     then echo -e "Directory $dir tidak valid.\n"
 else
-    dirjava=$(find $dir -type f -name "*.sh" -exec dirname {} \; | sort -u)
+    dirjava=$(find $dir -type f -name "*.java" -exec dirname {} \; | sort -u)
     if [[ `echo $dirjava | grep / | wc -l` -gt 0 ]]
     then
         printf '%s\n' $dirjava | awk '{print "Ada file Java pada direktori", $0}'
+    else 
+        echo "Tidak ditemukan file Java di dalam direktori $dir"
     fi
 fi
