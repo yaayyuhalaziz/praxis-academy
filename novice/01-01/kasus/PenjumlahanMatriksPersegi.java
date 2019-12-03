5e21d11b8ce52e2715a5f260680b8ed658f18c66
yaayyuhalaziz <yaayyuhalaziz@gmail.com>
2019-12-03 15:40:01 +0700

    resolve kasus 2

--- novice/01-01/kasus/PenjumlahanMatriksPersegi.java
+++ novice/01-01/kasus/PenjumlahanMatriksPersegi.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class PenjumlahanMatriksPersegi {
     public int[][] setMatriks(int baris, int[][] arr) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("\nMasukkan elemen matrik berukuran " + baris + " x " + baris + "\n");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan elemen matrik berukuran " + baris + " x " + baris + "\n");
         for(int i = 1; i <= baris; i++) {
             for(int j = 1; j <= baris; j++) {
                 System.out.print("Elemen ["+i+"]["+j+"]: ");
-                arr[i][j] = input.nextInt();
+                arr[i][j] = in.nextInt();
             }
         }
         return arr;
@@ -35,9 +35,18 @@ public class PenjumlahanMatriksPersegi {
         Scanner input = new Scanner(System.in);
         System.out.print("Penjumlahan matrik Bujur Sangkar n x n.\nTuliskan jumlah kolom atau baris: ");
         int baris = input.nextInt();
-        int[][] matriksA = new int[baris][baris];
-        int[][] matriksB = new int[baris][baris];
+        int[][] matriksA = new int[baris+1][baris+1];
+        int[][] matriksB = new int[baris+1][baris+1];
         PenjumlahanMatriksPersegi opsMatriks = new PenjumlahanMatriksPersegi();
+        System.out.println("\nMatriks A\n");
         matriksA = opsMatriks.setMatriks(baris, matriksA);
+        System.out.println("\nMatriks B\n");
+        matriksB = opsMatriks.setMatriks(baris, matriksB);
+        System.out.println("\nMatriks A\n");
+        opsMatriks.getMatriks(baris, matriksA);
+        System.out.println("\nMatriks B\n");
+        opsMatriks.getMatriks(baris, matriksB);
+        System.out.println("\nPenjumlahan keduanya menghasilkan Matriks:\n");
+        opsMatriks.penjumlahanMatriks(matriksA, matriksB, baris);
     }
 }
