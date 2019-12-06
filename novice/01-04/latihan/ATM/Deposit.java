8554c458494cbb374f9de685d1628a7b3c373a0f
yaayyuhalaziz <yaayyuhalaziz@gmail.com>
2019-12-06 16:00:55 +0700

    Latihan Kasus ATM

---
+++ novice/01-04/latihan/ATM/Deposit.java
@@ -0,0 +1,9 @@
+public class Deposit extends ATMMachine {
+    static double deposit;
+    public void setDeposit(double d) {
+        deposit = d;
+    }
+    public static double getDeposit() {
+        return deposit;
+    }
+}
