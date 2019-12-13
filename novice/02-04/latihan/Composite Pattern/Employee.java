876a46553fdb68e4ca4f061d0f29b35f40baf884
yaayyuhalaziz <yaayyuhalaziz@gmail.com>
2019-12-13 14:35:35 +0700

    Contoh Penggunaan Design Composite Pattern

---
+++ novice/02-04/latihan/Composite Pattern/Employee.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Employee
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public String toString() {
+        return ("Employee :[ Name : " + name + ", dept : " + dept + ", salary :" + salary+" ]");
+    }
+}
